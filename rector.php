95602c5f2d2141c4483a7c12afeaac4082c886c8
Tastaturberuf <mail@tastaturberuf.de>
2025-06-02 14:02:45 +0200

    feat: update Rector config to include `DeclareStrictTypesRector` and adjust

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -12,16 +13,29 @@ return RectorConfig::configure()
         __DIR__ . '/contao',
         __DIR__ . '/src',
     ])
-    ->withFileExtensions(['php', 'html5'])
     // uncomment to reach your current PHP version
     ->withPhpSets()
     ->withPreparedSets(
         true,
         true,
         true,
-        typeDeclarations: true
+        true,
+        true,
+        false,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true
     )
     ->withSkip([
+        DeclareStrictTypesRector::class => [
+            __DIR__ . '/contao/templates/*.html5'
+        ],
         FlipTypeControlToUseExclusiveTypeRector::class,
         RemoveUnusedPrivateMethodParameterRector::class => [
             __DIR__ . '/src/DataContainer/FilesDataContainer.php',
