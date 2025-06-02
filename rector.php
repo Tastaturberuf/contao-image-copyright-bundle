ccdb8ab2dbf1254c407786f69e0f29ab33a78807
Tastaturberuf <mail@tastaturberuf.de>
2025-06-02 13:30:26 +0200

    feat: add Contao Manager Plugin support, update Rector config, and enhance type declarations

--- rector.php
+++ rector.php
@@ -6,10 +6,13 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/contao',
         __DIR__ . '/src',
     ])
+    ->withFileExtensions(['php', 'html5'])
     // uncomment to reach your current PHP version
     ->withPhpSets()
-    ->withTypeCoverageLevel(0)
+    ->withPreparedSets(typeDeclarations: true)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -27,7 +27,7 @@ class Plugin implements BundlePluginInterface
     /**
      * @inheritDoc
      */
-    public function getBundles(ParserInterface $parser)
+    public function getBundles(ParserInterface $parser): array
     {
         return [
             BundleConfig::create(TastaturberufContaoImageCopyrightBundle::class)
