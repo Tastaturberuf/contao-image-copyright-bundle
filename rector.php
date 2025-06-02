1cbdd38f2ddbde91fe44a9f4f532ca6fee814af4
Tastaturberuf <mail@tastaturberuf.de>
2025-06-02 13:56:13 +0200

    feat: update Rector configuration with new rules and improve string formatting in `getImages` method

--- rector.php
+++ rector.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -13,6 +15,15 @@ return RectorConfig::configure()
     ->withFileExtensions(['php', 'html5'])
     // uncomment to reach your current PHP version
     ->withPhpSets()
-    ->withPreparedSets(typeDeclarations: true)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withPreparedSets(
+        true,
+        true,
+        true,
+        typeDeclarations: true
+    )
+    ->withSkip([
+        FlipTypeControlToUseExclusiveTypeRector::class,
+        RemoveUnusedPrivateMethodParameterRector::class => [
+            __DIR__ . '/src/DataContainer/FilesDataContainer.php',
+        ]
+    ]);
